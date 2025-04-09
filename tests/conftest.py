67927ea7c8b25b92993408e64c28936d56bfcdef
Caua-miguel <caua.sudol@outlook.com>
2025-04-08 23:59:16 -0300

    test: testing data insertion in route /filiais

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,7 @@
 import pytest
-from project import app_flask, load_config
-from database.models.branches import db
+from project import app_flask
 
 @pytest.fixture(scope="module")
 def app():
     "Instance of Main flask app"
-    app_flask.config.from_object(load_config)
-
-    with app_flask.app_context():
-        db.create_all() 
-    
-    yield app_flask
-    
-    with app_flask.app_context():
-        db.drop_all()
-
     return app_flask
--- tests/test_app.py
+++ tests/test_app.py
@@ -7,8 +7,9 @@ def test_config_is_loaded(config):
 def test_request_home_returns_201(client):
     assert client.get("/").status_code == 201
 
+def test_request_branches_add_success_returns_201(client):
+    # Usar mock para simular o request dessa rota
+    assert client.get("/filiais").status_code == 201
+
 def test_request_branches_already_exists_returns_409(client):
     assert client.get("/filiais").status_code == 409
-
-# def test_request_branches_add_success_returns_201(client):
-#     assert client.get("/filiais").status_code == 201
